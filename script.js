2f5bf7fdc6f29def6c051eba2b6d8480e7c4db5f
Ashish Kumar <48367301+Ashishrock9394@users.noreply.github.com>
2023-08-18 14:10:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,6 @@
 function decimalToBinary(num) {
-
-	let binary=0;
-	while (num > 0) {
-		let rem = num%2;
-		binary = binary*10 + rem;
-		num = Math.floor(num/2);
-	}
-
-	return binary;
+	
+	return Integer.toString(num,2);
 }
 
 window.decimalToBinary = decimalToBinary;
