a86254679a404f176b8e0693d36de24234624649
Ashish Kumar <48367301+Ashishrock9394@users.noreply.github.com>
2023-08-18 13:52:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,15 +2,15 @@ function decimalToBinary(num) {
 
 	let binary= num;
 	while (num > 0) {
-   if (num & 1) {
-      binary = "1" + binary;
-   } else {
-      binary = "0" + binary;
-   }
-   num = num >> 1;
-}
+	   if (num & 1) {
+	      binary = "1" + binary;
+	   } else {
+	      binary = "0" + binary;
+	   }
+	   num = num >> 1;
+	}
 
-	
+	return binary;
 }
 
 window.decimalToBinary = decimalToBinary;
